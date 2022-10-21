470bad4e5e783ebb00a4183a984be4bdb779f0df
jcesarmobile <jcesarmobile@gmail.com>
2022-10-21 20:02:48 +0200

    feat!: Remove dummy web implementation (#8)

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import type { ScreenBrightnessPlugin } from './definitions';
 
 const ScreenBrightness = registerPlugin<ScreenBrightnessPlugin>(
   'ScreenBrightness',
-  {
-    web: () => import('./web').then(m => new m.ScreenBrightnessWeb()),
-  },
+  {},
 );
 
 export * from './definitions';
