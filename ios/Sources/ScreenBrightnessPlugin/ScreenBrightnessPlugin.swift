b2cdfde458c981c981086014253437182df92d0e
jcesarmobile <jcesarmobile@gmail.com>
2024-11-21 17:48:45 +0100

    feat: Use SPM project structure (#20)

--- ios/Sources/ScreenBrightnessPlugin/ScreenBrightnessPlugin.swift
+++ ios/Sources/ScreenBrightnessPlugin/ScreenBrightnessPlugin.swift
@@ -6,7 +6,13 @@ import Capacitor
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(ScreenBrightnessPlugin)
-public class ScreenBrightnessPlugin: CAPPlugin {
+public class ScreenBrightnessPlugin: CAPPlugin, CAPBridgedPlugin {
+    public let identifier = "ScreenBrightnessPlugin"
+    public let jsName = "ScreenBrightness"
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "setBrightness", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getBrightness", returnType: CAPPluginReturnPromise)
+    ]
 
     @objc func setBrightness(_ call: CAPPluginCall) {
         let brightness = call.getFloat("brightness", Float(UIScreen.main.brightness))
