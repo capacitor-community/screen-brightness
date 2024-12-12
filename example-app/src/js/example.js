425de12cc8eaf49e46e9fdf59f2701b55f3c5052
jcesarmobile <jcesarmobile@gmail.com>
2024-12-12 17:58:57 +0100

    chore: Add real sample app

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,6 +1,18 @@
-import { ScreenBrightness } from '@capacitor-community&#x2F;screen-brightness';
+import { ScreenBrightness } from '@capacitor-community/screen-brightness';
 
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    ScreenBrightness.echo({ value: inputValue })
+window.reset = () => {
+    console.log('reset value');
+    const brightness = -1;
+    ScreenBrightness.setBrightness({ brightness })
+}
+
+window.updateValue = () => {
+    const brightness = parseFloat(document.getElementById("brightnessInput").value);
+    console.log('set value to', brightness);
+    ScreenBrightness.setBrightness({ brightness });
+}
+
+window.getBrigtness = async () => {
+    const value = await ScreenBrightness.getBrightness();
+    alert(value.brightness);
 }
